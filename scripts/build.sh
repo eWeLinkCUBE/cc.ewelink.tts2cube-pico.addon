1dfbf5482f0fc0e0e7aa954d44cff53e7e620905
Tony Yuan <tony.yuan@coolkit.cn>
2023-04-21 16:29:16 +0800

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+read -p "Build version: "
+build_version=$REPLY
+
 rm -rf build && mkdir build
 
 npx lerna run build
@@ -9,3 +12,8 @@ cp -r packages/server/dist build/server
 cp -r packages/web/dist build/public
 
 cp docker/Dockerfile build
+
+cat << EOF > ./build/buildinfo
+BUILD_VERSION=$build_version
+BUILD_DATE=$(date '+%F %T')
+EOF
